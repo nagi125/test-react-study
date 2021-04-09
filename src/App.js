47064370fa86fde77430e118127dbcf174de98f6
nagi125 <nagi125.dev@gmail.com>
2021-04-09 17:33:39 +0900

    default propsのサンプル

--- src/App.js
+++ src/App.js
@@ -17,14 +17,15 @@ const App = () => {
 
   const profiles = [
     { name: "Taro", age: 10 },
-    { name: "Hanako", age: 5 }
+    { name: "Hanako", age: 5 },
+    { name: "NoName"}
   ]
 
   return (
       <div>
         {
-          profiles.map((profile) => {
-            return <User name={profile.name} age={profile.age} />
+          profiles.map((profile, index) => {
+            return <User name={profile.name} age={profile.age} key={index}/>
           })
         }
       </div>
@@ -32,8 +33,12 @@ const App = () => {
 }
 
 const User = (props) => {
-
   return <div>Hi, I am {props.name}, and {props.age} years old!</div>
+}
+
+// 入力されてなかった場合に自動で入力してくれる。
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
