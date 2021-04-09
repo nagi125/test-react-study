f1fa4289e161441ee2c1e06ddab363b13d70fbc5
nagi125 <nagi125.dev@gmail.com>
2021-04-09 17:56:38 +0900

    stateの表示

--- src/App.js
+++ src/App.js
@@ -1,51 +1,27 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-// クラスコンポーネント
-// class App extends Component {
-//   render() {
-//     return (
-//       <React.Fragment>
-//         <label htmlFor={"bar"}>bar</label>
-//         <input type={"text"} onClick={() => { console.log('hoge');}} />
-//       </React.Fragment>
-//     )
-//   }
-// }
+import React, { Component } from 'react';
 
 // 関数コンポーネント
 const App = () => {
 
-  const profiles = [
-    { name: "Taro", age: 10 },
-    { name: "Hanako", age: 5 },
-    { name: "NoName"}
-  ]
-
-  return (
-      <div>
-        {
-          profiles.map((profile, index) => {
-            return <User name={profile.name} age={profile.age} key={index}/>
-          })
-        }
-      </div>
-  )
+  return (<Counter></Counter>)
 }
 
-const User = (props) => {
-  return <div>Hi, I am {props.name}, and {props.age} years old!</div>
-}
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0}
+  }
 
-// 入力されてなかった場合に自動で入力してくれる。
-User.defaultProps = {
-  age: 1
-}
+  render() {
+    return (
+        <React.Fragment>
+          <div>counter: {this.state.count}</div>
+          <button>+1</button>
+          <button>-1</button>
+        </React.Fragment>
+    )
 
-// Propsの型を定義してあげる。
-User.propTypes = {
-  name: PropTypes.string,
-  age:  PropTypes.number.isRequired
+  }
 }
 
 export default App;
