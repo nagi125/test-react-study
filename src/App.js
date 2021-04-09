994c517d3db26d15c5ed7bba4ef0f5f34212eff4
nagi125 <nagi125.dev@gmail.com>
2021-04-09 16:42:06 +0900

    label追加

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import { Component } from 'react';
 
 class App extends Component {
   render() {
-    const greeting = "Hi!"
-    const dom = <h1 className="foo">{greeting}</h1>
-    return dom;
+    return (
+      <div>
+        <label htmlFor={"bar"}>bar</label>
+        <input type={"text"} onClick={() => { console.log('hoge');}} />
+      </div>
+    )
   }
 }
 
