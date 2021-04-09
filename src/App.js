0b6cb4f1ec1c9c5e603b4a04812abbec64738286
nagi125 <nagi125.dev@gmail.com>
2021-04-09 17:16:44 +0900

    関数コンポーネントサンプル追加

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React, { Component } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <React.Fragment>
-        <label htmlFor={"bar"}>bar</label>
-        <input type={"text"} onClick={() => { console.log('hoge');}} />
-      </React.Fragment>
-    )
-  }
+// クラスコンポーネント
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor={"bar"}>bar</label>
+//         <input type={"text"} onClick={() => { console.log('hoge');}} />
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+// 関数コンポーネント
+const App = () => {
+  return (
+      <div>
+        < Cat/>
+        < Cat/>
+        < Cat/>
+      </div>
+  )
+}
+
+const Cat = () => {
+  return <div>Meow!</div>
 }
 
 export default App;
