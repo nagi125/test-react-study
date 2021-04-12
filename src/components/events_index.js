27b06be00083fbd3e0b9f1fb58f0538f4461c3b3
nagi125 <nagi125.dev@gmail.com>
2021-04-12 17:42:00 +0900

    ファイルのリネーム

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -3,7 +3,7 @@ import { connect } from 'react-redux'
 
 import { increment, decrement} from "../actions";
 
-class App extends Component {
+class EventsIndex extends Component {
   render() {
     const props = this.props
 
@@ -23,4 +23,4 @@ const mapDispatchToProps = dispatch => ({
   decrement: () => dispatch(decrement())
 })
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default connect(mapStateToProps, mapDispatchToProps)(EventsIndex)
